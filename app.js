51d7cb0dd402690be27565302bade87a7719a492
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2022-05-16 18:54:04 +0300

    new changes
    About jumping, running news fix, contact me fix

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ menu.addEventListener("click", function() {
     menu.classList.toggle("is-active");
     menuLinks.classList.toggle("active");
 });
+
+document.querySelector("#contact-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = "";
+    e.target.elements.email.value = "";
+    e.target.elements.message.value = "";
+});
